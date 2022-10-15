280f08e2d7c6946298dabf5510382a9f9c4c83b7
Divje Bhandari <divjebhandari@gmail.com>
2022-10-16 02:58:39 +0300

    correct

--- Java homework/src/main/java/io/codelex/questions/exercise3/JoinerApp.java
+++ Java homework/src/main/java/io/codelex/questions/exercise3/JoinerApp.java
@@ -2,9 +2,10 @@ package io.codelex.questions.exercise3;
 
 public class JoinerApp {
     public static void main(String[] args) {
-        Joiner join = new Joiner<>("-");
+        Joiner<Integer> join = new Joiner<>("-");
+        Joiner<String> join2 = new Joiner<>("-");
 
-        System.out.println(join.joinThem(54));
-        System.out.println(join.joinThem("FWGWG", 4343143, 1515151, "hello", 4544));
+        System.out.println(join2.joinThem("yoo", "wsuup","Gunit"));
+        System.out.println(join.joinThem(4324, 4343143, 1515151, 414, 4544));
     }
 }
